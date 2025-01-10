804cf483369126bf1e8d9ebc45f20fd1509331ff
ayushroy64 <ayushroy165@gmail.com>
2025-01-09 22:30:17 -0800

    removed desktop 3d

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[100px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[130px]  max-w-10xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
@@ -18,13 +18,13 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className='text-[#915EFF]'>Ayush</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Developer. Explorer. Learner
+          <p className={`${styles.heroSubText} mt-10 text-white-100`}>
+            Developer. Explorer. Learner.
           </p>
         </div>
       </div>
 
-      <ComputersCanvas />
+      
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
